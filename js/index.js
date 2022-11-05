d7abdc5667b00f8a1713b0b0833487a148d8b31b
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2022-11-05 15:18:24 +0600

    global temp chart

--- js/index.js
+++ js/index.js
@@ -4,12 +4,102 @@
 //   return response.text();
 // }
 
+const ctx = document.querySelector(".js-chart").getContext("2d");
+// console.log(ctx);
+const globalTemp = 14;
+
+fetchData()
+  .then(parseData)
+  .then(getLabelandData)
+  .then(({ years, temps }) => drawChart(years, temps));
+
 function fetchData() {
-  fetch("./ZonAnn.Ts+dSST.csv")
-    .then((response) => {
-      return response.text();
-    })
-    .then((data) => console.log(Papa.parse(data, { header: true }).data));
+  return fetch("./ZonAnn.Ts+dSST.csv").then((response) => {
+    return response.text();
+  });
 }
 
-fetchData();
+function parseData(data) {
+  return Papa.parse(data, { header: true }).data;
+}
+
+function getLabelandData(data) {
+  return data.reduce(
+    (acc, entry) => {
+      acc.years.push(entry.Year);
+      acc.temps.push(Number(entry.Glob) + globalTemp);
+      return acc;
+    },
+    { years: [], temps: [] }
+  );
+}
+
+function drawChart(labels, data) {
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "# Global temp",
+          data: data,
+          backgroundColor: ["black"],
+          borderColor: ["yellow"],
+          borderWidth: 1,
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+}
+
+// .then((data) => {
+// const parseData = Papa.parse(data, { header: true }).data;
+
+//       const mapperData = parseData.reduce(
+//         (acc, entry) => {
+//           acc.years.push(entry.Year);
+//           acc.temps.push(Number(entry.Glob) + globalTemp);
+//           return acc;
+//         },
+//         { years: [], temps: [] }
+//       );
+
+//       console.log(mapperData);
+
+//       //   const years = parseData.map((entry) => entry.Year);
+//       //   const temps = parseData.map((entry) => Number(entry.Glob) + globalTemp);
+//       //   console.log(years);
+//       //   console.log(temps);
+
+//       new Chart(ctx, {
+//         type: "line",
+//         data: {
+//           labels: mapperData.years,
+//           datasets: [
+//             {
+//               label: "# Global temp",
+//               data: mapperData.temps,
+//               backgroundColor: ["black"],
+//               borderColor: ["yellow"],
+//               borderWidth: 1,
+//               fill: false,
+//             },
+//           ],
+//         },
+//         options: {
+//           scales: {
+//             y: {
+//               beginAtZero: false,
+//             },
+//           },
+//         },
+//       });
+//     });
